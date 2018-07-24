07f23b68cdd587cfbc2f42ea351eb349212d0348
PGajewski <p.gajewski@stud.elka.pw.edu.pl>
2018-07-24 12:28:56 +0200

    Add Program.cpp

---
+++ Program.cpp
@@ -0,0 +1,10 @@
+#include "Program.h"
+
+Program::Program()
+{
+
+}
+Program::~Program()
+{
+
+}
---
+++ Program.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <thread>
+
+class Program : public std::thread
+{
+private:
+	std::string name;
+	std::string icone_path;
+public:
+	Program(std::string name_s, std::string path) : name(name_s), icone_path(path) {}
+	inline std::string getName() { return this->name; }
+	inline std::string getIcone() { return this->icone_path; }
+
+	Program();
+	~Program();
+
+	/*Functions to handle signals from operational system*/
+	virtual void OnLeftKeyPressed() = 0;
+	virtual void OnLeftKeyReleased() = 0;
+
+	virtual void OnRightKeyPressed() = 0;
+	virtual void OnRightKeyReleased() = 0;
+
+	virtual void OnUpKeyPressed() = 0;
+	virtual void OnUpKeyReleased() = 0;
+
+	virtual void OnDownKeyPressed() = 0;
+	virtual void OnDownKeyReleased() = 0;
+
+	virtual void OnPressKeyPressed() = 0;
+	virtual void OnPressKeyReleased() = 0;
+
+	virtual void OnKey1Pressed() = 0;
+	virtual void OnKey1Released() = 0;
+
+	virtual void OnKey2Pressed() = 0;
+	virtual void OnKey2Released() = 0;
+
+	virtual void OnKey3Pressed() = 0;
+	virtual void OnKey3Released() = 0;
+
+	virtual void OnExit() = 0;
+
+	/*Thread function - pure virtual*/
+	virtual void operator()() = 0;
+};
