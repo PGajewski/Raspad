cf4d23aeef833640fe0644210020d0707dba2c9f
Paweł Gajewski <269823@pw.edu.pl>
2018-07-31 02:48:45 +0200

    First modify FileManager class

---
+++ obj/FileManager.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Program.h"
+
+class FileManager : public Program
+{
+	/*Functions to handle signals from operational system*/
+	void OnLeftKeyPressed();
+	void OnLeftKeyReleased();
+
+	void OnRightKeyPressed();
+	void OnRightKeyReleased();
+
+	void OnUpKeyPressed();
+	void OnUpKeyReleased();
+
+	void OnDownKeyPressed();
+	void OnDownKeyReleased();
+
+	void OnPressKeyPressed();
+	void OnPressKeyReleased();
+
+	void OnKey1Pressed();
+	void OnKey1Released();
+
+	void OnKey2Pressed();
+	void OnKey2Released();
+
+	void OnKey3Pressed();
+	void OnKey3Released();
+
+	void OnExit();
+
+	/*Thread function - pure virtual*/
+	void operator()();
+};
