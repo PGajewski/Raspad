cd6fef112cc8075e49c12065d4b0052477a3493f
Paweł Gajewski <269823@pw.edu.pl>
2018-07-31 03:59:48 +0200

    Correct errors

---
+++ obj/split_string.h
@@ -0,0 +1,10 @@
+#pragma once
+template <class Container>
+void mySplit(const std::string& str, Container& cont, char delim = ' ')
+{
+	std::stringstream ss(str);
+	std::string token;
+	while (std::getline(ss, token, delim)) {
+		cont.push_back(token);
+	}
+}
