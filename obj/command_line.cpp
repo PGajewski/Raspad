67ee345d3e09e987b6ea359767fceeb639ce0164
Paweł Gajewski <269823@pw.edu.pl>
2018-08-06 01:51:45 +0200

    Correct errors

---
+++ obj/command_line.cpp
@@ -0,0 +1,20 @@
+#include "command_line.h"
+
+std::string exec(const char* cmd) {
+	char buffer[128];
+	std::string result = "";
+	FILE* pipe = popen(cmd, "r");
+	if (!pipe) throw std::runtime_error("popen() failed!");
+	try {
+		while (!feof(pipe)) {
+			if (fgets(buffer, 128, pipe) != NULL)
+				result += buffer;
+		}
+	}
+	catch (...) {
+		pclose(pipe);
+		throw;
+	}
+	pclose(pipe);
+	return result;
+}
