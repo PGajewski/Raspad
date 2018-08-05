4f6fa43e0d40661d88841dbd777054e7b50d1eea
Paweł Gajewski <269823@pw.edu.pl>
2018-08-06 01:25:34 +0200

    Correct errors

--- obj/FileManager.h
+++ obj/FileManager.h
@@ -18,9 +18,9 @@ private:
 	std::string getActualPath();
 	void updateDirectoryContent();
 	void printDirectoryContent();
-	std::string FileManager::getSizeOfFile(std::string& path) const;
+	std::string getSizeOfFile(std::string& path) const;
 	std::string getFileDescription(std::string& path) const;
-	std::string FileManager::getNumberOfFilesInDirectory(std::string& path) const;
+	std::string getNumberOfFilesInDirectory(std::string& path) const;
 
 	std::atomic_bool wasChange{true};
 
