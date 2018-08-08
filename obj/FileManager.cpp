3115ed8438aa522c379c748eefaa12b95b1e0df4
PGajewski <p.gajewski@stud.elka.pw.edu.pl>
2018-08-08 14:05:21 +0200

    Correct errors

--- obj/FileManager.cpp
+++ obj/FileManager.cpp
@@ -86,7 +86,7 @@ void FileManager::updateActualChosenOption()
 std::string FileManager::getFileDescription(const std::string& path) const
 {
 	std::stringstream command;
-	command << "file \"" << path << "\" | awk '{$1= \"\"; print $0}'";
+	command << "file -F '' \"" << path << "\" | sed s:\"" << path << "\"::";
 	return exec(command.str().c_str());
 }
 
